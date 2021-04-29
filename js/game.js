12b06e33996cb701f435853cbf3fe1cbf2a01eda
ChiranjeeviShinde <74903894+ChiranjeeviShinde@users.noreply.github.com>
2021-04-29 19:27:49 +0530

    Add files via upload

--- js/game.js
+++ js/game.js
@@ -62,8 +62,8 @@ class Game{
 
                      textSize(25);
                      fill(255);
-                     text("Player 1 : " + allPlayers.player1.score,70,50);
-                     text("Player 2 : " + allPlayers.player2.score,70,100);
+                     text("Player 1 : " + allPlayers.player1.score,50,50);
+                     text("Player 2 : " + allPlayers.player2.score,50,100);
                     
                  }
 
